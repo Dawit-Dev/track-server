f25a467509f5a093217bbfef682eb68470e9d38f
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-05-31 15:18:08 +0100

    Implementee Async Storage

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,43 +1,49 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import createDataContext from "./createDataContext";
-import trackerApi from '../api/tracker';
-// import { response } from "express";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
-    switch (action.type) {
-        case 'add_error':
-            return { ...state, errorMessage: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { errorMessage: "", token: action.payload };
+    default:
+      return state;
+  }
 };
 
-const signup = dispatch => {
-    return async ({ email, password }) => {
-         try {
-             const response = await trackerApi.post('/signup', { email, password })
-             console.log(response.data)
-         } catch (error) {
-             dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
-         }
-     }
- }
+const signup =
+  (dispatch) =>
+  async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      await AsyncStorage.setItem("token", response.data.token);
+      dispatch({ type: "signup", payload: response.data.token });
+    } catch (error) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with sign up",
+      });
+    }
+  };
 
 const signin = (dispatch) => {
-    return ({ email, password }) => {
-        // Try to signin
-        // Handle success by updating state
-        // Handle failure by showing error message (somehow)
-    };
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  };
 };
 
 const signout = (dispatch) => {
-    return () => {
-        // somehow sign out!!;
-    };
+  return () => {
+    // somehow sign out!!;
+  };
 };
 
 export const { Provider, Context } = createDataContext(
-    authReducer,
-    { signin, signout, signup },
-    { isSignIn: false, errorMessage: '' }
+  authReducer,
+  { signin, signout, signup },
+  { token: null, errorMessage: "" }
 );
