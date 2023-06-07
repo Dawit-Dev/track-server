f4fba069229d2ab5537bef39c37731802e4acb44
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-06-07 17:10:44 +0100

    Drawing a Position Indicator

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import { StyleSheet, Text, ActivityIndicator } from "react-native";
+import MapView, { Polyline, Circle } from "react-native-maps";
 import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
@@ -11,7 +11,6 @@ const Map = () => {
   }
 
   return (
-    <>
       <MapView
         style={styles.map}
         initialRegion={{
@@ -26,8 +25,14 @@ const Map = () => {
         //   latitudeDelta: 0.01,
         //   longitudeDelta: 0.01,
         // }}
-      ></MapView>
-    </>
+      >
+        <Circle
+          center={currentLocation.coords}
+          radius={30}
+          strokeColor="rgba(158, 158, 255, 1.0)"
+          fillColor="rgba(158, 158, 255, 0.3)"
+        />
+      </MapView>
   );
 };
 
