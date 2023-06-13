4c6548ae583fb48701d74de3284edea64c299cfd
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-06-13 13:03:16 +0100

    Bring back the Polyline

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -4,35 +4,36 @@ import MapView, { Polyline, Circle } from "react-native-maps";
 import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
-  const { state: { currentLocation } } = useContext(LocationContext);
-  
+  const {
+    state: { currentLocation, locations },
+  } = useContext(LocationContext);
+
   if (!currentLocation) {
-    return <ActivityIndicator size='large' style={{ marginTop: 200 }} />
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
 
   return (
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          ...currentLocation.coords,
-          // latitude: 37.33233,
-          // longitude: -122.03121,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-        // region={{
-        //   ...currentLocation.coords,
-        //   latitudeDelta: 0.01,
-        //   longitudeDelta: 0.01,
-        // }}
-      >
-        <Circle
-          center={currentLocation.coords}
-          radius={30}
-          strokeColor="rgba(158, 158, 255, 1.0)"
-          fillColor="rgba(158, 158, 255, 0.3)"
-        />
-      </MapView>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      // region={{
+      //   ...currentLocation.coords,
+      //   latitudeDelta: 0.01,
+      //   longitudeDelta: 0.01,
+      // }}
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+      <Polyline coordinates={locations.map((loc) => loc.coords)} />
+    </MapView>
   );
 };
 
